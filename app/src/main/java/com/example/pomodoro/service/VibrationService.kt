132c3ad6a95e459918ca287249c18c1b5efd64a4
hwang434 <inqbator@knou.ac.kr>
2022-08-20 21:58:36 +0900

    진동 코드 리팩토링 메서드로 분리화하고, 서비스가 종료되면 진동과 음악 재생이 멈추게 설정.

--- app/src/main/java/com/example/pomodoro/service/VibrationService.kt
+++ app/src/main/java/com/example/pomodoro/service/VibrationService.kt
@@ -10,13 +10,46 @@ import android.util.Log
 import com.example.pomodoro.R
 
 class VibrationService: Service(){
-    private val TAG: String = "로그"
+    companion object {
+        private const val TAG: String = "로그"
+    }
     private lateinit var vibrator: Vibrator
     private lateinit var musicPlayer: MediaPlayer
 
     override fun onCreate() {
         Log.d(TAG,"VibrationService - onCreate() called")
         super.onCreate()
+        createVibrator()
+        createMediaPlayer()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d(TAG,"VibrationService - onStartCommand() called")
+        startMusicMedia()
+        startVibrate()
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopVibrate()
+        stopMusicMedia()
+    }
+
+    private fun stopMusicMedia() {
+        musicPlayer.stop()
+    }
+
+    private fun stopVibrate() {
+        vibrator.cancel()
+    }
+
+    private fun createVibrator() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val vibrateManager = getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
             vibrator = vibrateManager.defaultVibrator
@@ -24,19 +57,17 @@ class VibrationService: Service(){
             @Suppress("DEPRECATION")
             vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         }
+    }
 
+    private fun createMediaPlayer() {
         musicPlayer = MediaPlayer.create(this, R.raw.ppippi)
     }
 
-    override fun onBind(intent: Intent?): IBinder? {
-        Log.d(TAG,"VibrationService - onBind() called")
-        return null
+    private fun startVibrate() {
+        musicPlayer.start()
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d(TAG,"VibrationService - onStartCommand() called")
-        musicPlayer.start()
-
+    private fun startMusicMedia() {
         vibrator.vibrate(
             VibrationEffect.createWaveform(longArrayOf(0, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500), -1),
             AudioAttributes.Builder().
@@ -44,7 +75,5 @@ class VibrationService: Service(){
             setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).
             build()
         )
-
-        return super.onStartCommand(intent, flags, startId)
     }
 }
